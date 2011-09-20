6cce296c48588c150bbee9d20177152c77d67854
guo <global user.email sept_guo@163.com>
2011-09-20 08:31:08 +0800

    a

--- aka/game.c
+++ aka/game.c
@@ -16,7 +16,7 @@ int creat_num(int *a)
         if (flag[temp] == 0) 
         {
             a[i] = temp + '0';
-  //      printf("%5d", a[i]-'0');
+            //      printf("%5d", a[i]-'0');
             i++;
             flag[temp] = 1;
         }
@@ -33,70 +33,78 @@ int judge_num(int *ptr)
     int i,j;
     int A = 0;
     int B = 0;
+    pid_t pid;
 
-    while (flag < 5)
+    pid = fork();
+    if (pid > 0) 
     {
-        A = 0;
-        B = 0;
-     be:   printf("please input four numbers from 0 to 9\n");
-        fgets(buf, sizeof(buf), stdin);
-        buf[strlen(buf)-1] = '\0';
-        if (strlen(buf) != 4) 
+        sleep(40);
+        printf("sorry failed~!\n");
+        exit(-1);
+    }
+    else if (pid == 0)
+    {
+        while (flag < 5)
         {
-            printf("error input~!\n");
-            continue;
-        }
-        for (i = 0; i < 4; i++)
-        {
-            if (buf[i] < '0' || buf[i] > '9') 
+            A = 0;
+            B = 0;
+        be: printf("please input four numbers from 0 to 9\n");
+            fgets(buf, sizeof(buf), stdin);
+            buf[strlen(buf)-1] = '\0';
+            if (strlen(buf) != 4) 
             {
-                printf("please input numbers\n");
-                goto be;
+                printf("error input~!\n");
+                continue;
             }
-            for (j = i+1; j < 4; j++)
+            for (i = 0; i < 4; i++)
             {
-                if (buf[i] == buf[j]) 
+                if (buf[i] < '0' || buf[i] > '9') 
                 {
-                    printf("error ~! input \n");
+                    printf("please input numbers\n");
                     goto be;
                 }
+                for (j = i+1; j < 4; j++)
+                {
+                    if (buf[i] == buf[j]) 
+                    {
+                        printf("error ~! input \n");
+                        goto be;
+                    }
+                }
             }
-        }
-        for (i = 0; i < 4; i++)
-        {
-     //   printf("a[i]=%d b[i] = %d\n", ptr[i], buf[i]);
-            if (ptr[i] == buf[i]) 
+            for (i = 0; i < 4; i++)
             {
-                A++;  
+                //   printf("a[i]=%d b[i] = %d\n", ptr[i], buf[i]);
+                if (ptr[i] == buf[i]) 
+                {
+                    A++;  
+                }
             }
-        }
-        for (i = 0; i < 4; i++)
-        {
-             for (j = 0; j < 4; j++)
-             {
-                 if (ptr[i] == buf[j]) 
-                 {
-                     B++;
-                 }
-             }
-        }
-        printf("%dA%dB\n", A, B);
-        if (A == 4) 
-        {
-            printf("congratulations ~!\n");
-            exit(0);
-        }
-        flag++;
-        
-    }    
-    printf("sorry failed~!\n");
-    return 0;
+            for (i = 0; i < 4; i++)
+            {
+                for (j = 0; j < 4; j++)
+                {
+                    if (ptr[i] == buf[j]) 
+                    {
+                        B++;
+                    }
+                }
+            }
+            printf("%dA%dB\n", A, B);
+            if (A == 4) 
+            {
+                printf("congratulations ~!\n");
+                exit(1);
+            }
+            flag++;
+        }    
+    }
 
+    return 0;
 }
 int main(int argc, char *argv[])
 {
     int array[4];
-
     creat_num(array);
     judge_num(array);
 
